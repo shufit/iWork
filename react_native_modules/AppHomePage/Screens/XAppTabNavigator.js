198871d82b0d71d81309ce8e866e992c501c454b
ShiXiaoHui <548446009@qq.com>
2018-05-15 17:46:42 +0800

    登录页面UI编写

--- react_native_modules/AppHomePage/Screens/XAppTabNavigator.js
+++ react_native_modules/AppHomePage/Screens/XAppTabNavigator.js
@@ -41,7 +41,7 @@ const AppTabScreenNavigator = TabNavigator({
     tabPageTwo:{
         screen:XAppSecondTabScreen,
         navigationOptions:{
-            title:'发现',
+            title:'工作台',
             tabBarVisible:true,
             swipeEnabled:false,
             tabBarIcon:({focused,tintColor })=>{
@@ -69,7 +69,7 @@ const AppTabScreenNavigator = TabNavigator({
     tabPageThree:{
         screen:XAppThirdTabScreen,
         navigationOptions:{
-            title:'生活',
+            title:'门户',
             tabBarVisible:true,
             swipeEnabled:false,
             tabBarIcon:({focused,tintColor })=>{
@@ -127,12 +127,12 @@ const AppTabScreenNavigator = TabNavigator({
     swipeEnabled: false, // 禁止左右滑动
     backBehavior: 'none', // 按 back 键是否跳转到第一个 Tab， none 为不跳转
     tabBarOptions: {
-        activeTintColor: '#008AC9', // 文字和图片选中颜色
-        inactiveTintColor: '#999', // 文字和图片默认颜色
+        activeTintColor: 'blue', // 文字和图片选中颜色
+        inactiveTintColor: '#fff', // 文字和图片默认颜色
         showIcon: true, // android 默认不显示 icon, 需要设置为 true 才会显示
         indicatorStyle: {height: 0}, // android 中TabBar下面会显示一条线，高度设为 0 后就不显示线了
         style: {
-            backgroundColor: '#fff', // TabBar 背景色
+            backgroundColor: '#0079d7', // TabBar 背景色
         },
         labelStyle: {
             fontSize: 12, // 文字大小
--- react_native_modules/AppLogin/Screens/LoginScreen.js
+++ react_native_modules/AppLogin/Screens/LoginScreen.js
@@ -8,7 +8,10 @@ import {
     Button,
     View,
     Image,
-    TouchableOpacity
+    TouchableOpacity,
+    ScrollView,
+    ImageBackground,
+    TextInput,
 } from 'react-native';
 
 import {
@@ -42,7 +45,10 @@ class LoginScreen extends Component {
     });
 
     constructor(props) {
-        super(props)
+        super(props);
+        this.userName = '';
+        this.password = '';
+
     }
 
     componentDidMount() {
@@ -51,7 +57,77 @@ class LoginScreen extends Component {
 
 
     render() {
-        return (<View style={styles.container}/>);
+        return (
+            <View style={styles.container}>
+                <ImageBackground style={styles.backgroundImg}
+                       source={require('../Images/login_background_img.png')}
+                       resizeMode={'cover'}
+                >
+                    <View style={styles.inputContainerView}>
+                        <Text style={styles.labelText}>{'用户名'}</Text>
+                        <View style={styles.verticalLine}/>
+                        <TextInput
+                            style={styles.textInput}
+                            placeholder={'请输入你的工号'}
+                            onChangeText={(text)=>{
+                                this.userName = text;
+                            }}
+                        />
+                    </View>
+
+                    <View style={styles.inputContainerView}>
+                        <Text style={styles.labelText}>{'密   码'}</Text>
+                        <View style={styles.verticalLine}/>
+                        <TextInput
+                            style={styles.textInput}
+                            placeholder={'初始密码为Of+身份证后6位'}
+                            secureTextEntry={true}
+                            padding={0}
+                            underlineColorAndroid={'transparent'}
+                            onChangeText={(text)=>{
+                                this.password = text;
+                            }}
+                        />
+                    </View>
+
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity onPress={()=>{
+                            this._loginAction();
+                        }}>
+                            <View style={styles.loginButton}>
+                                <Text style={styles.loginButtonText}>{'登录'}</Text>
+                            </View>
+
+                        </TouchableOpacity>
+
+                        <TouchableOpacity onPress={()=>{
+                            this._findPwdAction();
+                        }}>
+                            <Text style={styles.findPwdButtonText}>{'找回密码'}</Text>
+                        </TouchableOpacity>
+
+                    </View>
+
+
+                </ImageBackground>
+            </View>
+        );
+    }
+
+
+    /*
+    ***登录
+     */
+    _loginAction() {
+
+    }
+
+    /*
+    ***找回密码
+     */
+
+    _findPwdAction() {
+
     }
 
 
@@ -60,9 +136,79 @@ class LoginScreen extends Component {
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        backgroundColor:'red',
+        backgroundColor:'#fff',
+    },
+    backgroundImg: {
+        flex:1,
+        width:60 * a,
+        flexDirection:'column',
+        alignItems:'center',
+        paddingTop: 20 * a,
+    },
+    inputContainerView:{
+        width:45 * a,
+        height:6 * a,
+        borderWidth:1,
+        borderColor:'#0079d7',
+        borderRadius: 2 * a,
+        backgroundColor:'transparent',
+        flexDirection:'row',
+        alignItems:'center',
+        marginTop: 4 * a,
 
+    },
+    labelText:{
+        fontSize:14,
+        fontWeight:'100',
+        color:'#0079d7',
+        marginLeft:2 * a,
+        marginRight:0.5* a,
+        width: 8 * a,
+    },
+    textInput:{
+        height: 5 * a,
+        width: 33 * a,
+        backgroundColor:'transparent',
+        borderWidth:0,
+        marginLeft: 1.5 * a,
+    },
+    buttonContainer:{
+        flex:1,
+        flexDirection:'column',
+        alignItems:'flex-end',
+
+    },
+    loginButton: {
+        width:45 * a,
+        height:6 * a,
+        backgroundColor:'#0079d7',
+        borderRadius:2 * a,
+        marginTop: 6 * a,
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    loginButtonText:{
+        fontSize:20,
+        color:'#fff',
+    },
+    findPwdButtonText:{
+        fontSize: 12,
+        color:'#0079d7',
+        marginTop:3 * a,
+    },
+    //分割横线
+    dividingLine: {
+        backgroundColor: '#E8E8E8',
+        height: 0.5,
+        width: 60 * a * 0.9
+    },
+    //分割竖线
+    verticalLine: {
+        backgroundColor: '#0079d7',
+        height: 14,
+        width: 0.5
     }
+
 
 });
 
