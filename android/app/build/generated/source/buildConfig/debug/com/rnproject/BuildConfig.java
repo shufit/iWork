d29085c27bc3363ccfade5cad3adfc4c624efc36
ShiXiaoHui <548446009@qq.com>
2018-05-19 10:56:47 +0800

    修改android端gradle配置文件

--- android/app/build/generated/source/buildConfig/debug/com/rnproject/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/debug/com/rnproject/BuildConfig.java
@@ -5,7 +5,7 @@ package com.rnproject;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.rnproject";
+  public static final String APPLICATION_ID = "com.ofilm.iwork";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
